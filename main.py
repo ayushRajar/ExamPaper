13b0573084bc875ea045d581f1b33b1976b1d846
ayushRajar <theayushraj.ar@outlook.com>
2023-12-18 10:32:02 +0530

    Modified The Main For paper Generation

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ wb=openpyxl.load_workbook(filename="Q2.xlsx")
 sheet= wb.active
 
 
-"""print("WELCOME TO QUESTION PAPER GENERATOR: ")
+print("WELCOME TO QUESTION PAPER GENERATOR: ")
 
 if(__name__=="__main__"):
     course = input("ENTER THE COURSE NAME: ")
@@ -42,24 +42,24 @@ if(__name__=="__main__"):
                 l=[f"{i}{chr(97+j)}",q,m]
                 csvwriter.writerow(l)
 
-    csvfile.close()"""
+    csvfile.close()
 
-print("PREPARING YOUR QUESTION PAPER: ")
-with open("Question.Csv" ,"r") as csvfile:
-    csvreader=csv.reader(csvfile)
-    data_item=[]
-    for data in csvreader:
-        data_item.append(data)
-csvfile.close()
+    print("PREPARING YOUR QUESTION PAPER: ")
+    with open("Question.Csv" ,"r") as csvfile:
+        csvreader=csv.reader(csvfile)
+        data_item=[]
+        for data in csvreader:
+            data_item.append(data)
+    csvfile.close()
 
-print(data_item)
-ques_col=15
-for data in range(1,len(data_item)):
-    sheet[f"A{ques_col}"]=data_item[data][0]
-    sheet[f"B{ques_col}"] = data_item[data][1]
-    sheet[f"D{ques_col}"] = data_item[data][2]
-    ques_col+=1
-wb.save(filename="Q#.xlsx")
+    print(data_item)
+    ques_col=15
+    for data in range(1,len(data_item)):
+        sheet[f"A{ques_col}"]=data_item[data][0]
+        sheet[f"B{ques_col}"] = data_item[data][1]
+        sheet[f"D{ques_col}"] = data_item[data][2]
+        ques_col+=1
+    wb.save(filename="Q#.xlsx")
 
 
 
